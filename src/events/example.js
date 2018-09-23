b55a6975c22ab25d47fa994f686c3ce6af70da5d
JustStayAlive <juststayalive@riseup.net>
2018-09-23 17:58:54 -0400

    Added an example event

---
+++ src/events/example.js
@@ -0,0 +1,10 @@
+module.exports = {
+  event: {
+    id: 'exampleMessageCreate',
+    event: 'messageCreate',
+    type: 'on',
+    execute: async (message) => { // eslint-disable-line no-unused-vars
+
+    }
+  }
+};
