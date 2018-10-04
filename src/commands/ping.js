535f50ddd6db20dd651ece72a44ff140818afbc8
JustStayAlive <juststayalive@riseup.net>
2018-10-04 02:50:10 -0400

    Added a ping command

---
+++ src/commands/ping.js
@@ -0,0 +1,36 @@
+module.exports = {
+  command: {
+    id: 'ping',
+    trigger: ['ping', 'pong'],
+    execute: async (client, content, author, guild, channel, message, prefix, trigger, arguments) => { // eslint-disable-line no-unused-vars
+      let response = await channel.createMessage({
+        embed: {
+          color: 12335146,
+          title: (trigger === 'ping') ? 'Pong!':'Ping!'
+        }
+      });
+
+      response.edit({
+        embed: {
+          color: 12335146,
+          title: response.embeds[0].title,
+          thumbnail: {
+            url: client.user.avatarURL
+          },
+          fields: [
+            {
+              name: 'Latency',
+              value: `${response.timestamp - message.timestamp}ms`,
+              inline: true
+            },
+            {
+              name: 'API Latency',
+              value: `${client.requestHandler.latencyRef.latency}ms`,
+              inline: true
+            }
+          ]
+        }
+      });
+    }
+  }
+};
